b4ece8c8f36848e36f8abcc238c12a2c6f84ff2f
Kalaiselvan S <kalaiselvan.selvaraj@gmail.com>
2024-11-21 02:00:06 +0530

    Add files via upload
    Asynchronous and Non Blocking Reactive Programming

---
+++ webflux-nonblocking/src/main/java/com/mycom/samples/webflux/WebfluxNonblockingApplication.java
@@ -0,0 +1,13 @@
+package com.mycom.samples.webflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebfluxNonblockingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebfluxNonblockingApplication.class, args);
+	}
+
+}
